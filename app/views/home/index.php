b869235304f5de0900c60aea1bc3344aa1fabb3a
axe1-ja <85787359+axe1-ja@users.noreply.github.com>
2022-10-04 12:00:53 +0200

    Home Setup: - Mise en place de la premiere page - Ajout des logos et images necessaires

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,48 +3,49 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <link rel="icon" type="image/x-icon" href="../public/resources/storage/Images/logo.ico">
+    <title>J.E.R.A.L.D.</title>
     
+    <link rel="stylesheet" href="./resources/css/app.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
     
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                </ul>
+
+    <img src="../public/resources/storage/Images/fond-home.jpg" class="w-100 background-image" alt="">
+    
+    <div class="container text-center p-4">
+        <div class="row">
+            <div class="col">
+                <h1>Hello, <?=$data['name']?>!</h1>
             </div>
         </div>
-    </nav>
-    <h1>Hello, <?=$data['name']?>!</h1>
+        <div class="row home-elements">
+            <div class="col">
+                <h5 id="homelink-left" class="text-white">
+                    <i class="bi bi-caret-left"></i>
+                    Accès Visiteurs
+                </h5>
+            </div>
+            <div class="col"> 
+                <div class="center logo-container">
+                    <img src="../public/resources/storage/Images/logo-app.png" class="logo" alt="">
+                </div>
+            </div>
+            <div class="col">
+                <h5 id="homelink-left" class="text-white">
+                    <i class="bi bi-caret-right"></i>
+                    Accès Membres
+                </h5>
+            </div>
+        </div>
+    </div>
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-
 </body>
 </html>
