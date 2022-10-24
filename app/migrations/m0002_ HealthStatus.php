fcb61c36f59249f346bcf2cbed6b756f4e8843ab
HJoonY <yooh9942@gmail.com>
2022-10-24 17:34:31 +0200

    database HealthStatus add

---
+++ app/migrations/m0002_ HealthStatus.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class m0001_users {
+
+    
+
+    public function up($pdo){
+        echo PHP_EOL.'Applying migration'.PHP_EOL;
+        $db = $pdo;
+        $SQL = "CREATE TABLE HealthStatus(
+            Health_Id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+            Health_HeartBeat INT NOT NULL,
+            Health_Soundlevel INT NOT NULL,
+            Health_Temperature FLOAT,
+            Health_datetime DateTime,
+            User_id INT,
+            foreign key (User_id) references users(User_id)
+            ) 
+        ENGINE=INNODB;";
+        $db->exec($SQL);
+    }
+
+    public function down(){
+        echo 'Down migration'.PHP_EOL;
+        $db = \app\core\App::$app->db;
+        $SQL = "DROP TABLE HealthStatus;";
+        $db->pdo->exec($SQL);
+    }
+}
