2a26769c89230282b6a1cf39bfd22ea3854022e2
HJoonY <yooh9942@gmail.com>
2022-11-15 08:58:44 +0100

    changment de port pour juste joon

--- app/migrations.php
+++ app/migrations.php
@@ -1,13 +1,13 @@
 <?php
-require_once 'app\core\App.php';
-require_once 'app\core\Database.php';
+require_once 'app/core/App.php';
+require_once 'app/core/Database.php';
 
 
 $config = [];
-$e=explode(" ", file_get_contents('.env'));
+/*$e=explode(" ", file_get_contents('.env'));
 foreach($e as $f) {
     $config[explode('=',$f)[0]]=explode('=',$f)[1];
-}
+}*/
 $app = new App($config);
 
 $app->db->applyMigrations();
