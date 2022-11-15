76682ec834fd90865705f7c1da6eb3c11e83a848
axe1-ja <85787359+axe1-ja@users.noreply.github.com>
2022-11-15 08:55:07 +0100

    Axel modifs 21 - correction migration romain - correction fichier auth (n'avait pas de .php)

--- app/migrations/m0008_Measure.php
+++ app/migrations/m0008_Measure.php
@@ -10,8 +10,8 @@ class m0008_Measure {
         $SQL = "CREATE TABLE Measure(
             Measure_Id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
             Measure_Value INT(250) NOT NULL,
-            Mesaure_Datetime INT(1000) NOT NULL,
             Sensor_Id INT,
+            Measure_Datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             foreign key (Sensor_Id) references sensor(Sensor_Id)
             ) 
         ENGINE=INNODB;";
