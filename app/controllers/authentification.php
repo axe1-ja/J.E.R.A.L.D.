7c4f69a784f48467bd344cd3a8434b394a347ac6
axe1-ja <85787359+axe1-ja@users.noreply.github.com>
2022-11-29 10:36:34 +0100

    Axel_modifs_24 - mise en place du système de variables de session - dès entrée sur le site, utilisateur est considéré comme 'guest' avec un is de 0     - quand fonction de login sera en place, on mettera le nom de l'utilisateur et son id

--- app/controllers/authentification.php
+++ app/controllers/authentification.php
@@ -22,9 +22,9 @@ class Authentification extends Controller
         $query = "INSERT INTO users (User_nom, User_Prenom, User_email, User_phone, User_address, User_password) VALUES ('".$user->nom."', '".$user->prenom."', '".$user->email."', '".$user->phone."', '".$user->adress."', '".$user->password."')";
         $statement = $db->pdo->prepare($query);
         $statement->execute();
-        $db->pdo;
+        //$db->pdo;
 
-        $this->view('home', []);
+        $this->view('authentification/index', []);
 
     }
 
@@ -55,126 +55,4 @@ class Authentification extends Controller
         $this->view('authentification/reset_password', []);
     }
 
-}
-
-
-class Member
-{
-
-    private $ds;
-
-    function __construct()
-    {
-        require_once __DIR__ . '/../lib/DataSource.php';
-        $this->ds = new DataSource();
-    }
-
-    /**
-     * to check if the email already exists
-     *
-     * @param string $email
-     * @return boolean
-     */
-    public function isEmailExists($email)
-    {
-        $query = 'SELECT * FROM users where email = ?';
-        $paramType = 's';
-        $paramValue = array(
-            $email
-        );
-        $resultArray = $this->ds->select($query, $paramType, $paramValue);
-        $count = 0;
-        if (is_array($resultArray)) {
-            $count = count($resultArray);
-        }
-        if ($count > 0) {
-            $result = true;
-        } else {
-            $result = false;
-        }
-        return $result;
-    }
-
-    /**
-     * to signup / register a user
-     *
-     * @return string[] registration status message
-     */
-    public function registerMember()
-    {
-        $isEmailExists = $this->isEmailExists($_POST["email"]);
-        if ($isEmailExists) {
-            $response = array(
-                "status" => "error",
-                "message" => "Email already exists."
-            );
-        } else {
-            if (! empty($_POST["signup-password"])) {
-
-                $hashedPassword = password_hash($_POST["signup-password"], PASSWORD_DEFAULT);
-            }
-            $query = 'INSERT INTO users (name, email, phone, adress, password) VALUES (?, ?, ?, ?, ?)';
-            $paramType = 'sss';
-            $paramValue = array(
-                $_POST["name"],
-                $_POST["mail"],
-                $_POST["phone"],
-                $_POST["adress"],
-                $hashedPassword
-            );
-            $memberId = $this->ds->insert($query, $paramType, $paramValue);
-            if (! empty($memberId)) {
-                $response = array(
-                    "status" => "success",
-                    "message" => "You have registered successfully."
-                );
-            }
-        }
-        return $response;
-    }
-
-    public function getMember($email)
-    {
-        $query = 'SELECT * FROM users where email = ?';
-        $paramType = 's';
-        $paramValue = array(
-            $email
-        );
-        $memberRecord = $this->ds->select($query, $paramType, $paramValue);
-        return $memberRecord;
-    }
-
-    /**
-     * to login a user
-     *
-     * @return string
-     */
-    public function loginMember()
-    {
-        $memberRecord = $this->getMember($_POST["email"]);
-        $loginPassword = 0;
-        if (! empty($memberRecord)) {
-            if (! empty($_POST["login-password"])) {
-                $password = $_POST["login-password"];
-            }
-            $hashedPassword = $memberRecord[0]["password"];
-            $loginPassword = 0;
-            if (password_verify($password, $hashedPassword)) {
-                $loginPassword = 1;
-            }
-        } else {
-            $loginPassword = 0;
-        }
-        if ($loginPassword == 1) {
-            // login sucess so store the member's email in the session
-            session_start();
-            $_SESSION["email"] = $memberRecord[0]["email"];
-            session_write_close();
-            $url = "./home.php";
-            header("Location: /views/user");
-        } else if ($loginPassword == 0) {
-            $loginStatus = "Invalid username or password.";
-            return $loginStatus;
-        }
-    }
 }
