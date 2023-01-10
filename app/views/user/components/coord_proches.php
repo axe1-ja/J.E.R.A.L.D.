a714205e4c12b5dd6a8992654051a8d606ae24bf
Elena <elena.charpentier@eleve.isep.fr>
2023-01-10 12:11:20 +0100

    charpentier elena coord proche style

--- app/views/user/components/coord_proches.php
+++ app/views/user/components/coord_proches.php
@@ -1,7 +1,7 @@
 <div class="container mt-3">
-    <div class="row">
+<div class="row">
         <div class="col-6">
-            <div class="card center" style="width: 50rem;">
+            <div class="card center" style="width: 25rem;">
                 <div class="card-body">
                     <h4 class="card-title text-center">Contact 1</h4>
                     <div class="row">
@@ -15,13 +15,13 @@
                     </div>
                     <br>
                     <div class="row">
-                        <div class="col-4">Téléphone</div>
+                        <div class="col-4">Télephone</div>
                         <div class="col-8">06 45 93 27 05</div>
                     </div>
                 </div>
             </div>
         </div>
-        
+
 
         <div class="col-6">
             <div class="card center" style="width: 25rem;">
@@ -38,14 +38,13 @@
                     </div>
                     <br>
                     <div class="row">
-                        <div class="col-4">Téléphone</div>
+                        <div class="col-4">Télephone</div>
                         <div class="col-8">06 45 93 27 05</div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <div class="row mt-3 text-center">
         <div class="col-12">
             <h3 class="text-white fw-bold">En cas de chute, nous appellerons vos deux proches ci-dessus.</h3>
