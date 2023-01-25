e27996f184e732c9b7868905237e03cb306317b8
axe1-ja <axeljosephantoine@gmail.com>
2023-01-25 18:41:35 +0100

    11

--- app/controllers/DatabaseController.php
+++ app/controllers/DatabaseController.php
@@ -7,7 +7,6 @@ class DatabaseController extends Controller
         //dd($_POST);
         if(isset($_POST['password']) && $_POST['password']=='migration80123_#987jerald%kfed'){
             $db = new Database();
-            dd('aa');
             $db->applyMigrations();
 
             return $this->view('database/migrate', [
