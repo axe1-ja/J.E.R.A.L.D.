817bdd68dd603147d6618f5caeb7d03dc8401e71
axe1-ja <axeljosephantoine@gmail.com>
2023-01-26 01:51:48 +0100

    ..

--- app/migrations/m0014_update_recoverycodes.php
+++ app/migrations/m0014_update_recoverycodes.php
@@ -4,7 +4,7 @@
 class m0014_update_recoverycodes {
     public function up($pdo){
         $db = $pdo;
-        $SQL = "ALTER TABLE recoverycode
+        $SQL = "ALTER TABLE RecoveryCode
         ADD recoverycode_code VARCHAR(300) NOT NULL;";
         $db->exec($SQL);
     }
